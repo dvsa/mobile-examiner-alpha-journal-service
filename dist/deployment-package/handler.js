8af014941da7c65434b2bb6a2d19826b1c3abf86
Alec Taylor <alec.taylor@bjss.com>
2018-03-26 14:17:03 +0100

    remove dist folder from git

--- dist/deployment-package/handler.js
+++
@@ -1,8 +0,0 @@
-import { getJournal } from './function/getJournal';
-import { setJournal } from './function/setJournal';
-var handler = {
-    getJournal: getJournal,
-    setJournal: setJournal
-};
-export default handler;
-//# sourceMappingURL=handler.js.map
