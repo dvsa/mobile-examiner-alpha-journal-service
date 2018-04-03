db19f24789aefbc4ac8d1be401f7e15751b979bb
Freddie Hands <freddie.hands@MAC3379.local>
2018-04-03 09:44:25 +0100

    remove comments

--- src/utils/createResponse.ts
+++ src/utils/createResponse.ts
@@ -1,15 +1,3 @@
-// export default ({ body = {}, statusCode = 200 }) => {
-// 	const response = {
-// 		statusCode,
-// 		headers: {
-// 			'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-// 		},
-// 		body: JSON.stringify(body),
-// 	};
-// 	return response;
-// };
-
-
 import { IResponse } from "../interfaces/iresponse";
 
 export default (body: {}, statusCode = 200, reqHeaders: { [id: string]: string; } = {}): IResponse => {
