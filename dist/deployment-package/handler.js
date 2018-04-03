8290ebf4b45aa1c6ff4857cbf138371cd9de1d9f
Freddie Hands <freddie.hands@MAC3379.local>
2018-04-03 10:20:12 +0100

    remove dist folder

--- dist/deployment-package/handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var get_1 = require("./functions/get");
-var set_1 = require("./functions/set");
-var handler = {
-    get: get_1.get,
-    set: set_1.set
-};
-exports.default = handler;
