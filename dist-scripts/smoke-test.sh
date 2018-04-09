ad6cc5486aa224438fda8e66adf2d3ebc58c1f27
Michael Smyth <michaelsm@easter.kainos.com>
2018-04-09 14:50:46 +0100

    Removed unwanted -d payload in curl

--- dist-scripts/smoke-test.sh
+++ dist-scripts/smoke-test.sh
@@ -1,4 +1,4 @@
-http=$(curl -s -o /dev/null -w "%{http_code}" -i -X GET  -H "Content-Type: application/json" -d '{"candidateId": "someCandidateId","faults":[{"id":"juncSpeed","faultsNo":10,"isSerious":false,"isDangerous":false}]}' "${JOURNAL_API_URL}")
+http=$(curl -s -o /dev/null -w "%{http_code}" -i -X GET  -H "Content-Type: application/json" "${JOURNAL_API_URL}")
 echo "$http"
 
 if [[ "$http" == 200 ]];
