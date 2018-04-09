c2b95ad72efdd214f16e27a9f460498e6cd94e37
Michael Smyth <michaelsm@easter.kainos.com>
2018-04-09 14:28:50 +0100

    Removed unwanted line

--- dist-scripts/smoke-test.sh
+++ dist-scripts/smoke-test.sh
@@ -8,6 +8,3 @@ else
   echo "Unsuccessful"
   exit 1
 fi
-
-
-http=$(curl -s -o /dev/null -w "%{http_code}" -i -X GET  -H "Content-Type: application/json" -d '{"candidateId": "someCandidateId","faults":[{"id":"juncSpeed","faultsNo":10,"isSerious":false,"isDangerous":false}]}' https://931gfw2iw6.execute-api.eu-west-1.amazonaws.com/alpha/?email=test@test.com
